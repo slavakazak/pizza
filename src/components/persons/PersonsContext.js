f6e0b4d99c09272c06fc2a5b854a943289fef1c0
slavakazak <slavakazak3@gmail.com>
2022-10-11 19:17:50 +0300

    Added scroll to button

--- src/components/persons/PersonsContext.js
+++ src/components/persons/PersonsContext.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useEffect} from "react"
+import React, {useState, useContext, useEffect, useRef} from "react"
 
 const PersonsContext = React.createContext()
 
@@ -8,8 +8,20 @@ export const PersonsProvider = ({children}) => {
 
 	const [persons, setPersons] = useState(() => JSON.parse(localStorage.getItem('persons') || '[]'))
 
+	const previosPersonsLength = useRef(persons.length)
+
 	useEffect(() => {
     localStorage.setItem('persons', JSON.stringify(persons))
+		
+		if(persons.length - previosPersonsLength.current > 0){
+			let scrollPos = Math.max(
+				document.body.scrollHeight, document.documentElement.scrollHeight,
+				document.body.offsetHeight, document.documentElement.offsetHeight,
+				document.body.clientHeight, document.documentElement.clientHeight
+			) - document.documentElement.clientHeight
+			window.scrollTo(0, scrollPos)
+		}
+		previosPersonsLength.current = persons.length
   }, [persons])
 
 	function setPersonsWithSort(newPersons){
